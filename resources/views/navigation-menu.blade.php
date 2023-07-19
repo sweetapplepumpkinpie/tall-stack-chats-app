279943374a5c9d5e71edaa7dfc3e36427a537570
sweetapplepumpkinpie <sweetapplepumpkinpie@gmail.com>
2023-07-19 13:37:15 -0400

    added bottom nav to dashboard

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -149,6 +149,9 @@
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('chats') }}" :active="request()->routeIs('chats')">
+                {{ __('Chats') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
